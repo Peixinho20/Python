f310912f564f6dad9a2c4690f78be14a18317e49
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-03-12 07:46:01 -0300

    Add files via upload

---
+++ d002.py
@@ -0,0 +1,4 @@
+dia = input('Em qual dia você nasceu?')
+mes = input('Em qual mês você nasceu?')
+ano = input('Em qual ano você nasceu?')
+print('Você nasceu no dia', dia,'de', mes,'de', ano, '. Correto?')
