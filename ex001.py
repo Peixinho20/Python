959d740190e3dedda4d5761e9f254493c34a621e
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-03-13 21:15:12 -0300

    Create ex001.py

---
+++ ex001.py
@@ -0,0 +1,6 @@
+'''
+Faça um progrma que leia o nome de uma pessoas e mostre uma mensagem de boas vindas
+'''
+
+nome = input(str('Digite seu nome por favor: '))
+print('Seja bem-vindo(a)', nome, '!')
