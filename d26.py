f82b799dbb3985c2105ae1f8f173fe2dabfcb499
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-03-21 04:45:29 -0300

    Add files via upload

---
+++ d26.py
@@ -0,0 +1,7 @@
+'''
+Crie um programa que leia uma frase pelo teclado e mostre:
+
+quantas vezes aparece a letra 'A'
+em que posição ela aparece a primeira vez.
+em que posição ela aparece pela ultima vez.
+'''
---
+++ d27.py
@@ -0,0 +1,6 @@
+'''
+Faça um programa que leia o nome completo de um pessoas, mostrando em seguida o primeiro e o último nome separadamente
+Ex: Ana Maria de Souza
+primeiro: Ana
+último: Souza
+'''
