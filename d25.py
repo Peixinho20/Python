0db09c9896959b0febd5c858775f08dad1d27c53
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-04-02 01:19:16 -0300

    Update d25.py

--- d25.py
+++ d25.py
@@ -1,3 +1,5 @@
 '''
 Crie um programa que leia o nome da uma pessoa e diga se ela tem 'Silva' no nome.
 '''
+nome = str(input('Digite seu nome completo: ')).lower().strip()
+print('Você tem Silva no nome? {} '.format('silva' in nome))
