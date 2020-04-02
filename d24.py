e25e9cf17ade408f018dc37c016652fc2c4c6f83
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-04-02 01:17:41 -0300

    Update d24.py

--- d24.py
+++ d24.py
@@ -1,3 +1,5 @@
 '''
 Cire um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome 'Santo'.
 '''
+cidade = str(input('Digite a cidade em que você nasceu: ')).capitalize().split()
+print(cidade[0].endswith('Santo')) #diz se tem ou não o item procurado no item 0
