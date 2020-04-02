1917042fbcd80d0482b1bed46db165c8c68fe957
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-04-02 01:20:06 -0300

    Update d26.py

--- d26.py
+++ d26.py
@@ -5,3 +5,7 @@ quantas vezes aparece a letra 'A'
 em que posição ela aparece a primeira vez.
 em que posição ela aparece pela ultima vez.
 '''
+nome = str(input('Digite seu nome: ')).strip()
+print('A letra a aparece {} vezes'.format(nome.count('a')))
+print('A letra a aparece pela primeira vez na posição {}'.format(nome.find('a')))
+print('A letra a aparece pela última vez na posição {}'.format(nome.rfind('a')))
