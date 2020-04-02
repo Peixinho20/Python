39162d8d3d42270d41fa2e32b587b5089bfd8b58
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-04-02 01:27:33 -0300

    Update d27.py

--- d27.py
+++ d27.py
@@ -3,4 +3,8 @@ Faça um programa que leia o nome completo de um pessoas, mostrando em seguida o
 Ex: Ana Maria de Souza
 primeiro: Ana
 último: Souza
+#ATÉ A AULA 9
 '''
+nome = str(input('Digite seu nome completo: ')).split()
+print('Primeiro nome: {}'.format(nome[0]))
+print('Último nome: {}'.format(nome[-1]))
