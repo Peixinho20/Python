abb304c279d52c8a0ccdfb994939acae8e05d8be
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-04-02 01:14:28 -0300

    Update d22.py

--- d22.py
+++ d22.py
@@ -6,3 +6,14 @@ O nome com todas as letras minúsculas
 Quantas letras ao todo (sem considerar espaços)
 Quantas letras tem o primeiro nome
 '''
+nome = str(input('Digite seu nome completo: ')).strip() #retira espaços inuteis antes e depois da frase
+print('Analisando seu nome... ')
+print('Seu nome em maiúscula é {}.'.format(nome.upper()))
+print('Seu nome em minúscula é {}.'.format(nome.lower()))
+print('Seu nome ao todo tem {} letras.'.format(len(nome) - nome.count(' ')))
+dividido = nome.split()
+print('Seu primeiro nome tem {} letras'.format(len(dividido[0])))
+'''
+para a última pergunta: 
+print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
+'''
