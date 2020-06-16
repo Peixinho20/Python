c978a6e3ccc5d52c71711e53e765ac13e4a70585
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-06-16 04:09:15 -0300

    Delete d5.py

--- d5.py
+++
@@ -1,9 +0,0 @@
-'''
-Faça um programa que leia um número e mostre na tela o seu sucessor se antecessor.
-até a aula 7
-'''
-
-n = float(input('Digite um número: '))
-a = n+1
-b = n-1
-print('O sucessor dele é {} e o antecessor dele é {}.'.format(a,b))
