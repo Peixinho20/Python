36eb177c91093835d5605fe4ab9e27f6a195d6c8
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-06-16 04:02:22 -0300

    Delete d002.py

--- d002.py
+++
@@ -1,4 +0,0 @@
-dia = input('Em qual dia você nasceu?')
-mes = input('Em qual mês você nasceu?')
-ano = input('Em qual ano você nasceu?')
-print('Você nasceu no dia', dia,'de', mes,'de', ano, '. Correto?')
