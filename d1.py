3b8347b5c2939b8cdfae64b42240b840e900da64
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-06-16 04:05:39 -0300

    Delete d1.py

--- d1.py
+++
@@ -1,7 +0,0 @@
-# desafio 1
-'''
-Crie um script que python que leia o nome de uma pessoa e mostre uma mensagem
-de boas vindas de acorso com o valor digitado
-'''
-nome = input('Qual é o seu nome?')
-print('Olá,', nome, 'seja bem vind@!')
