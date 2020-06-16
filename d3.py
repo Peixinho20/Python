796b84ca5710812a994b6612bba11facb5ee165a
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-06-16 04:15:10 -0300

    Delete d3.py

--- d3.py
+++
@@ -1,8 +0,0 @@
-# exercício 3
-'''
-Crie um script python que leia dois números e tente mostra a soma entre eles
-'''
-x = int(input('Digite um valor: ')) # int é um tipo primitivo
-y = int(input('Digite outro valor: ')) #sem o int ocorre concatenaçao dos  valores
-c = x + y 
-print('A soma entre eles é ', c)
