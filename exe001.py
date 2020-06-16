2e68d409fdd774abab069544054bb20526bb52e2
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-06-16 04:07:35 -0300

    Delete exe001.py

--- exe001.py
+++
@@ -1,6 +0,0 @@
-'''
-Faça um progrma que leia o nome de uma pessoas e mostre uma mensagem de boas vindas
-'''
-
-nome = input(str('Digite seu nome por favor: '))
-print('Seja bem-vindo(a)', nome, '!')
