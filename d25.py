a5947063581e5eeb288e705e3a052880467e1f74
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-06-18 03:27:04 -0300

    Delete d25.py

--- d25.py
+++
@@ -1,5 +0,0 @@
-'''
-Crie um programa que leia o nome da uma pessoa e diga se ela tem 'Silva' no nome. #ATÉ A AULA 9
-'''
-nome = str(input('Digite seu nome completo: ')).lower().strip()
-print('Você tem Silva no nome? {} '.format('silva' in nome))
