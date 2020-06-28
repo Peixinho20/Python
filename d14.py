7375c814025e4278651b83a6e8282cbf0d4450b3
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-06-28 18:31:27 -0300

    Delete d14.py

--- d14.py
+++
@@ -1,8 +0,0 @@
-'''
-Crie um programa que converta uma temperatura digitada em °C e converta para °F.
-'''
-
-c = float(input('Informe a temperatura em °C: '))
-f = (9/5)*c + 32
-print('A temperatura {:.1f}°C corresponde a {:.1f}°F.'.format(c,f))
-
