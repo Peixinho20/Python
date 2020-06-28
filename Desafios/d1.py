533c534654733c6fb4f27b7c7ad6855d7141cfce
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-06-28 18:33:03 -0300

    Add files via upload

--- Desafios/d1.py
+++ Desafios/d1.py
@@ -1,7 +1,7 @@
 # desafio 1
 '''
 Crie um script que python que leia o nome de uma pessoa e mostre uma mensagem
-de boas vindas de acorso com o valor digitado
+de boas vindas ao curso com o valor digitado
 '''
 nome = input('Qual é o seu nome?')
-print('Olá,', nome, 'seja bem vind@!')
+print('\033[1;30;41mOlá,', nome, 'seja bem vind@!\033[m')
