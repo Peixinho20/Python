4f4318a83e32108f8af6b5a44245573efa539fec
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-06-28 18:30:49 -0300

    Delete d16.py

--- d16.py
+++
@@ -1,6 +0,0 @@
-'''
-Crie um programa que leia um número real qualquer pelo teclado e mostre na tela sua porção inteira. Até a aula 8
-'''
-from math import trunc
-n = float(input('Digite um número real qualquer: '))
-print('Aparte inteira do número digitado é {}'.format(trunc(n)))
