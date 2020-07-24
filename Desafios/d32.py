d73b91ae57c786282011efd4309287469340d245
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-07-24 01:58:23 -0300

    Delete d32.py

--- Desafios/d32.py
+++
@@ -1,9 +0,0 @@
-'''
-ATÉ AULA 10
-Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
-'''
-ano = int(input('Digite o ano desejado: '))
-if ano%4 == 0:
-    print('O ano \033[4;32;41m{}\033[m é bissexto.'.format(ano))
-else:
-    print('O ano \033[7;33;42m{}\033[m não é bissexto.'.format(ano))
