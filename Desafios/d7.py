20eb9689f715c7cdf8bba48210606d7371dcf949
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-07-24 01:56:11 -0300

    Delete d7.py

--- Desafios/d7.py
+++
@@ -1,9 +0,0 @@
-'''
-Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média.
-até a aula 7
-'''
-
-n1 = float(input('Digite a primeira nota: '))
-n2 = float(input('Digite a segunda nota: '))
-m = (n1 + n2)/2
-print('A média é \033[1;32;41m{:.2f}\033[m!'.format(m))
