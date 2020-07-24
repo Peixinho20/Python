5318f63d4b01033f1721c7ec904b243423a10936
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-07-24 14:10:53 -0300

    Delete d13.py

--- Desafios/d13.py
+++
@@ -1,6 +0,0 @@
-'''
-Faça um algoritmo que leia o salário de uma funcionário e mostre seu novo salário, com 15% de aumento. até a aula 7
-'''
-n = float(input('Digite o salário do funcionário: '))
-N = n + (n*15)/100
-print('O novo salário do funcionário é R$ \033[0;34;43m{:.2f}\033[m'.format(N))
