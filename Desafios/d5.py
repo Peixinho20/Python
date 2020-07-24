fcb2efd4b88245be2c47dbcf97c8a37fb33f4736
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-07-24 01:57:06 -0300

    Delete d5.py

--- Desafios/d5.py
+++
@@ -1,9 +0,0 @@
-'''
-Faça um programa que leia um número e mostre na tela o seu sucessor se antecessor.
-até a aula 7
-'''
-
-n = float(input('Digite um número: '))
-a = n+1
-b = n-1
-print('O sucessor dele é \033[7;36;45m{}\033[m e o antecessor dele é \033[1;37;46m{}\033[m.'.format(a,b))
