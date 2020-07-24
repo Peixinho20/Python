1d13dd3dbf0f7aa6c18c9c0ca60a1c1039ff998d
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-07-24 14:11:27 -0300

    Delete d10.py

--- Desafios/d10.py
+++
@@ -1,7 +0,0 @@
-'''
-Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares
-ela pode comprar. até a aula 7
-'''
-n = float(input('Quanto dinheiro vc tem carteira? '))
-d = n/5
-print('A quantidade em dólares é U$ \033[0;30;47m{}\033[m.'.format(d))
