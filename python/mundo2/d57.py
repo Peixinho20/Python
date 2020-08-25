fed94f1b9b2e99fe8e8daa273f5be82241a002db
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-08-25 00:27:54 -0300

    Delete d57.py

--- python/mundo2/d57.py
+++
@@ -1,9 +0,0 @@
-#Até a aula 14
-'''
-Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado,
-peça a digitação novamente até ter um valor correto.
-'''
-sexo = str(input('Digite seu sexo com M ou F: ')).strip().upper()[0]
-while sexo not in 'MmFf':
-    sexo = str(input('Voce digitou errado! Por favor informe seu sexo: ')).strip().upper()[0]
-print('Sexo {} registrado com sucesso!'.format(sexo))
