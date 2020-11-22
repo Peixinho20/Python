0b6c19740adec84904a4d332f93dad7a7321069e
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2020-11-22 11:11:35 -0300

    Add files via upload

--- python/mundo3/d75.py
+++ python/mundo3/d75.py
@@ -1,3 +1,4 @@
+
 #ATÉ A AULA 16
 '''
 Desenvolva um programa que leia quatro valores pelo teclado e guarde-os numa tupla. No final, mostre:
@@ -5,3 +6,21 @@ a) Quantas vezes apareceu o valor 9
 b) Em que posição foi digitado o primeiro valor 3.
 c) Quais foram os números pares.
 '''
+
+valores = (int(input('Digite um inteiro: ')),int(input('Digite outro inteiro: ')),
+           int(input('Digite mais um inteiro: ')),int(input('Digite o último inteiro: ')))
+print('-='*20)
+print(f'Você digitou os valores {valores}')
+print('-='*20)
+print(f'O valor 9 apareceu {valores.count(9)} vezes.')
+print('-='*20)
+if 3 in valores:
+    print(f'O valor 3 apareceu na posição {valores.index(3)+1}')
+else:
+    print('O valor 3 não foi digitado!')
+print('-='*20)
+print('O valores pares digitados foram: ', end='')
+for n in valores:
+    if n%2 == 0:
+        print(n, end=' ')
+
--- python/mundo3/d77.py
+++ python/mundo3/d77.py
@@ -1,4 +1,13 @@
 '''
-Crie um programa que tenha uma tupla com várias palavras(não usar acentos). Depois disso, você deve mostrar para cada
-palavra quais são as suas vogais.
+Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar
+para cada palavra quais são as suas vogais.
 '''
+
+frase = ('Preciso', 'passar', 'em', 'algebra', 'fundamentos', 'e', 'fisica')
+# o primeiro for analisa as palavras da frase
+for palavra in frase: #printar palavra me da as posições e printar frase me dá a frase para cada posição
+    print(f'\nNa palavra {palavra.upper()} temos ', end='')
+    # esse for analisa as letras em  cada palavra
+    for letra in palavra:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
